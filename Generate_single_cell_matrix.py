8018d746e26b861b1bb7648bf90106ba3177e566
ejackson054 <65126663+ejackson054@users.noreply.github.com>
2021-03-23 22:36:37 -0400

    Update Generate_single_cell_matrix.py
    Improved code annotation

--- Generate_single_cell_matrix.py
+++ Generate_single_cell_matrix.py
@@ -1,102 +1,108 @@
 import os
 import pandas as pd
+import sys
+
+
+"""USAGE:  python Generate_single_cell_matrix.py [base_folder] [fastq_1] [fastq_2]
 
 #Files you need in base folder:
 
-#1) Transcriptome (.fasta)
-#2) Barcodes (10xv3_whitelist.txt: downloaded from https://github.com/BUStools/getting_started/releases
-#3) t2g.py: downloaded from https://github.com/pachterlab/kallisto-transcriptome-indices/releases
+#1) t2g.py: downloaded from https://github.com/pachterlab/kallisto-transcriptome-indices/releases
+#2) Barcodes (10xv3_whitelist.txt): downloaded from https://github.com/BUStools/getting_started/releases
+#3) transcripts_to_genes.txt - generated in preprocessing
+#4) Transcriptome index (.index) -- generated in preprocessing
+
+Results will create an output directory called 'bus_output'
+
+"""
 
 
 """Assign variables"""
 
-fastq_folder = "/lab/solexa_public/Page/201118_WIGTC-NOVASEQ1A_AHFWHGDSXY/FASTQ"
-base_folder = "/lab/page_human_data/emily/single_cell"
+base_folder = sys.argv[1]
+fastq_1 = sys.argv[2]
+fastq_2 = sys.argv[3]
 
 
-"""Merge FASTQ files from different lanes"""
+"""Navigate to base folder and create output folders"""
 
-#Identify unique samples
-samples = []
-
-for i in os.listdir(fastq_folder):
-    
-    samples.append("{0}/{1}".format(fastq_folder,i.split("_L")[0]))
-    
-samples = list(set(samples))
-
-
-#Merge samples into single FASTQ files
-#os.system("mkdir {0}/fastq".format(base_folder))
-
-#for sample in samples:
-    
-#    reads_1 = "{0}_L001_R1_001.fastq.gz {0}_L002_R1_001.fastq.gz {0}_L003_R1_001.fastq.gz".format(sample)
-#    reads_2 = "{0}_L001_R2_001.fastq.gz {0}_L002_R2_001.fastq.gz {0}_L003_R2_001.fastq.gz".format(sample)
-    
-#    name = sample.split("/")[-1]
-    
-#    os.system("LSB_JOB_REPORT_MAIL=N bsub 'cat {0} > {1}/fastq/{2}_R1.fastq.gz'".format(reads_1, base_folder, name))
-#    os.system("LSB_JOB_REPORT_MAIL=N bsub 'cat {0} > {1}/fastq/{2}_R2.fastq.gz'".format(reads_2, base_folder, name))
-
-
-"""Run bustools"""
-
-#Generate raw output
 os.chdir(base_folder)
 
-
-#os.system("kallisto index -i Homo_sapiens.GRCh38.cdna.v100.all.fa.index Homo_sapiens.GRCh38.cdna.v100.all.fa")
-
-#os.system("python t2g.py --use_version < /lab/page/emily/Homo_sapiens.GRCh38.100.gtf > transcripts_to_genes.txt")
-
-
-fastq_list = sorted(os.listdir("{0}/fastq".format(base_folder)))
-
-to_drop = ["Undetermined_S0_R1.fastq.gz", "Undetermined_S0_R2.fastq.gz","L16_2577_S2_R1.fastq.gz","L16_2577_S2_R2.fastq.gz"]
-fastq_list = [i for i in fastq_list if i not in to_drop]
-
-fastq_list = " ".join(["{0}/fastq/{1}".format(base_folder,i) for i in fastq_list])
-
-os.system("kallisto bus -i Homo_sapiens.GRCh38.cdna.v100.all.fa.index -o bus_output/ -x 10xv3 -t 10 {0}".format(fastq_list))
-
-
-#Process results
-os.chdir("bus_output")
-
-os.system("bustools correct -w ../10xv3_whitelist.txt -o output.correct.bus output.bus")
-
-os.system("bustools sort -t 4 -o output.correct.sort.bus output.correct.bus")
-
-os.system("mkdir eqcount")
-os.system("mkdir genecount")
-
-os.system("bustools count -o eqcount/tcc -g ../transcripts_to_genes.txt -e matrix.ec -m -t transcripts.txt output.correct.sort.bus")
-
-os.system("bustools count -o genecount/gene -g ../transcripts_to_genes.txt -e matrix.ec -m -t transcripts.txt --genecounts output.correct.sort.bus")
-
-#Revise gene.genes.txt to have gene names
-t2g = pd.read_csv("{0}/transcripts_to_genes.txt".format(base_folder), sep = "\t", header=None)
-t2g = t2g.drop_duplicates([1])
-
-ensg = []
-symbols = []
-
-for i in range(0,len(t2g.index)):
+sample = fastq_1.split("/")[-1].split(".fastq")[0]
     
-    symbol = t2g.iloc[i,2]
-    ensg.append(t2g.iloc[i,1])
+if not os.path.isdir("{0}/bus_output".format(base_folder)):
+    os.system("mkdir bus_output")
+
+if not os.path.isdir("{0}/bus_output/{1}".format(base_folder,sample)):
+    os.system("mkdir bus_output/{0}".format(sample))
     
-    if symbol not in symbols:
-        symbols.append(symbol)
-    else:
-        symbols.append("{0}.1".format(symbol))
 
-t2g_dict = dict(zip(ensg,symbols)) 
+"""Do preprocessing, if necessary"""
+preprocessing = True
 
-with open("gene.genes.txt","r") as old:
-    with open("gene.genes.symbols.txt","w") as new:
-        for line in old:
-            new.write("{0}\n".format(t2g_dict[line.strip()]))
+if preprocessing:
+    
+    os.system("kallisto index -i hg38.gencode_v24.basic_ccds_nopar.transcripts.fa.index /lab/Page_lab-users/Alex/gtex/index/hg38.gencode_v24.basic_ccds_nopar.transcripts.fa")
+    os.system("python t2g.py < /lab/Page_lab-users/Alex/gtex/index/gencode.v24.annotation.basic_ccds_nopar.gtf > transcripts_to_genes.txt")
+
+
+"""Define functions"""
+        
+
+def main(base_folder, fastq_1, fastq_2):
+    
+    os.system("kallisto bus -i hg38.gencode_v24.basic_ccds_nopar.transcripts.fa.index -o bus_output/{0}/ -x 10xv3 -t 10 {1} {2}".format(sample, fastq_1, fastq_2))
+
+
+    #Process results
+    os.chdir("{0}/bus_output/{1}".format(base_folder,sample))
+
+    os.system("bustools correct -w ../../10xv3_whitelist.txt -o output.correct.bus output.bus")
+    
+    os.system("bustools sort -t 4 -o output.correct.sort.bus output.correct.bus")
+
+    os.system("mkdir eqcount")
+    os.system("mkdir genecount")
+
+    os.system("bustools count -o eqcount/tcc -g ../../transcripts_to_genes.txt -e matrix.ec -m -t transcripts.txt output.correct.sort.bus")
+
+    os.system("bustools count -o genecount/gene -g ../../transcripts_to_genes.txt -e matrix.ec -m -t transcripts.txt --genecounts output.correct.sort.bus")
+
+    #Revise gene.genes.txt to have gene symbols instead of Ensembl IDs
+    substitute_gene_symbols(base_folder, sample)
+    
+
+def substitute_gene_symbols(base_folder, sample):
+    
+    os.chdir("{0}/bus_output/{1}/genecount/".format(base_folder,sample))
+    t2g = pd.read_csv("{0}/transcripts_to_genes.txt".format(base_folder), sep = "\t", header=None)
+    t2g = t2g.drop_duplicates([1])
+    
+    ensg = []
+    symbols = []
+    
+    for i in range(0,len(t2g.index)):
+        symbol = t2g.iloc[i,2]
+        ensg.append(t2g.iloc[i,1])
+        if symbol not in symbols:
+            symbols.append(symbol)
+        else:
+            symbols.append("{0}.1".format(symbol))
+            
+    t2g_dict = dict(zip(ensg,symbols)) 
+    with open("gene.genes.txt","r") as old:
+        with open("gene.genes.symbols.txt","w") as new:
+            for line in old:
+                new.write("{0}\n".format(t2g_dict[line.strip()]))
+                
+    os.system("mv gene.genes.txt gene.genes.original.txt")
+    os.system("mv gene.genes.symbols.txt gene.genes.txt")
+
+
+
+"""Run"""
+if __name__ == "__main__":
+    
+    main(base_folder,fastq_1, fastq_2)
         
         
